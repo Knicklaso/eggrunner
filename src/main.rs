c918846164518272a2627631633a80b8e15cd8d6
Knicklaso <76531507+Knicklaso@users.noreply.github.com>
2025-12-03 12:29:41 -0500

    Add map generation and rendering

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,59 @@ use bracket_lib::prelude::*;
 use specs::prelude::*;
 use specs_derive::Component;
 
+const MAP_WIDTH: i32 = 80;
+const MAP_HEIGHT: i32 = 50;
+
+#[derive(PartialEq, Copy, Clone)]
+enum TileType {
+    Wall,
+    Floor,
+}
+
+struct Map {
+    tiles: Vec<TileType>,
+    width: i32,
+    height: i32,
+}
+
+impl Map {
+    fn xy_idx(&self, x: i32, y: i32) -> usize {
+        (y * self.width + x) as usize
+    }
+}
+
+fn new_map() -> Map {
+    let mut map = Map {
+        tiles: vec![TileType::Floor; (MAP_WIDTH * MAP_HEIGHT) as usize],
+        width: MAP_WIDTH,
+        height: MAP_HEIGHT,
+    };
+
+    for x in 0..map.width {
+        let idx_top = map.xy_idx(x, 0);
+        let idx_bottom = map.xy_idx(x, map.height - 1);
+        map.tiles[idx_top] = TileType::Wall;
+        map.tiles[idx_bottom] = TileType::Wall;
+    }
+
+    for y in 0..map.height {
+        let idx_left = map.xy_idx(0, y);
+        let idx_right = map.xy_idx(map.width - 1, y);
+        map.tiles[idx_left] = TileType::Wall;
+        map.tiles[idx_right] = TileType::Wall;
+    }
+
+    let mut rng = RandomNumberGenerator::new();
+    for _ in 0..400 {
+        let x = rng.range(1, map.width - 1);
+        let y = rng.range(1, map.height - 1);
+        let idx = map.xy_idx(x, y);
+        map.tiles[idx] = TileType::Wall;
+    }
+
+    map
+}
+
 #[derive(Component, Debug, Clone, Copy)]
 #[storage(VecStorage)]
 struct Position {
@@ -26,6 +79,12 @@ impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         ctx.cls();
 
+        let (map_width, map_height) = {
+            let map = self.ecs.fetch::<Map>();
+            draw_map(&map, ctx);
+            (map.width, map.height)
+        };
+
         let mut positions = self.ecs.write_storage::<Position>();
 
         let mouse_primary = {
@@ -40,8 +99,8 @@ impl GameState for State {
                 let dx = mouse_x - hero_pos.x;
                 let dy = mouse_y - hero_pos.y;
 
-                hero_pos.x = (hero_pos.x + dx.signum()).clamp(0, 79);
-                hero_pos.y = (hero_pos.y + dy.signum()).clamp(0, 49);
+                hero_pos.x = (hero_pos.x + dx.signum()).clamp(0, map_width - 1);
+                hero_pos.y = (hero_pos.y + dy.signum()).clamp(0, map_height - 1);
             }
         }
 
@@ -53,10 +112,24 @@ impl GameState for State {
     }
 }
 
+fn draw_map(map: &Map, ctx: &mut BTerm) {
+    for y in 0..map.height {
+        for x in 0..map.width {
+            let idx = map.xy_idx(x, y);
+            if map.tiles[idx] == TileType::Wall {
+                ctx.set(x, y, RGB::named(GREEN), RGB::named(BLACK), to_cp437('#'));
+            }
+        }
+    }
+}
+
 fn main() -> BError {
     let mut world = World::new();
     world.register::<Position>();
     world.register::<Renderable>();
+
+    let map = new_map();
+    world.insert(map);
 
     let hero = world
         .create_entity()
