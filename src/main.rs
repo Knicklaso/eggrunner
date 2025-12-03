a191a05c7ae99745e565f7e68ec11cf3198f21f5
Knicklaso <76531507+Knicklaso@users.noreply.github.com>
2025-12-03 10:36:31 -0500

    Move hero with right click

--- src/main.rs
+++ src/main.rs
@@ -19,13 +19,27 @@ struct Renderable {
 
 struct State {
     ecs: World,
+    hero: Entity,
 }
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         ctx.cls();
 
-        let positions = self.ecs.read_storage::<Position>();
+        let mut positions = self.ecs.write_storage::<Position>();
+
+        if ctx.mouse_button(MouseButton::Right) {
+            let (mouse_x, mouse_y) = ctx.mouse_pos();
+
+            if let Some(hero_pos) = positions.get_mut(self.hero) {
+                let dx = mouse_x - hero_pos.x;
+                let dy = mouse_y - hero_pos.y;
+
+                hero_pos.x = (hero_pos.x + dx.signum()).clamp(0, 79);
+                hero_pos.y = (hero_pos.y + dy.signum()).clamp(0, 49);
+            }
+        }
+
         let renderables = self.ecs.read_storage::<Renderable>();
 
         for (pos, render) in (&positions, &renderables).join() {
@@ -39,7 +53,7 @@ fn main() -> BError {
     world.register::<Position>();
     world.register::<Renderable>();
 
-    world
+    let hero = world
         .create_entity()
         .with(Position { x: 40, y: 25 })
         .with(Renderable {
@@ -69,7 +83,7 @@ fn main() -> BError {
         .with_fps_cap(30.0)
         .build()?;
 
-    let gs = State { ecs: world };
+    let gs = State { ecs: world, hero };
 
     main_loop(context, gs)
 }
